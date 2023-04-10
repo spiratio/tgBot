5c05590cd74c412b4f00c9a6d23be258ffb8b1bc
spiratio <sophia13@mail.ru>
2023-04-10 02:39:03 +0200

    fix: removed ./dist

--- dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DATABASE_TYPE = void 0;
-var DATABASE_TYPE;
-(function (DATABASE_TYPE) {
-    DATABASE_TYPE["MONGO"] = "mongo";
-    DATABASE_TYPE["REDIS"] = "redis";
-})(DATABASE_TYPE = exports.DATABASE_TYPE || (exports.DATABASE_TYPE = {}));
